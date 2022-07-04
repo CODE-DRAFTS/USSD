4850143eac5d653281d52c6171ab45f5a08b229d
bernard njeru <bernardnjerumtwaiti@gmail.com>
2022-07-04 15:30:40 +0300

    initial commit

--- app/schemas.py
+++ app/schemas.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 from typing import Optional
 
 class UssdInfo(BaseModel):
-    date:  str                              #date in UTC format(yyyy-mm-dd HH:mm:ss)
-    sessionId: str 
-    serviceCode: str                        #ussd code
-    networkCode: str                        #(63902 safaricom)  (620903 airtel kenya) (99999 sandbox)
-    phoneNumber: str                        # subscriber phone no
+    date:  Optional[str]                              #date in UTC format(yyyy-mm-dd HH:mm:ss)
+    sessionId: Optional[str] 
+    serviceCode: Optional[str]                      #ussd code
+    networkCode: Optional[str]                       #(63902 safaricom)  (620903 airtel kenya) (99999 sandbox)
+    phoneNumber: Optional[str]                        # subscriber phone no
     status: Optional[str]                   #incompelete, success,  failed, errorMessage
     cost: Optional[str]                     #cost of USSD session
     durationInMillis: Optional[str]
     hopsCount: Optional[str]                #steps user passedduring ussd session
-    input: str                              #input entered by user separeted by *
+    input: Optional[str]                             #input entered by user separeted by *
     lastAppResponse: Optional[str]
     errorMessage: Optional[str]
-    text:str                                #textual input
+    text:Optional[str]=''                               #textual input
